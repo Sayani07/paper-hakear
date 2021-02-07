c5c1cfc4929cca5b4c0e064bfc3b65af9f831b40
Sayani Gupta <Sayani.Gupta@monash.edu>
2021-02-07 23:13:30 +1100

    document updated

--- example_transformations.R
+++ example_transformations.R
@@ -267,7 +267,9 @@ N01 %>%
 ## attempt 3 (check if other distribution also leads to this)
 ## for this you need to change the input files and check if the coefficients are kind of same
 
-G21 <- read_rds("simulations/raw/null_design_quantrans/data-agg/all_data_wpd_Gamma01.rds")
+G21 <- read_rds("simulations/raw/null_design_quantrans/data-agg/all_data_wpd_N05.rds")
+
+G21$value %>% summary()
 
 G21_median <- G21 %>% 
   group_by(nx*nfacet) %>% 
