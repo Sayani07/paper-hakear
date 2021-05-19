68fe81fe692dcfba8bf921de02c62d5718dd970e
Sayani Gupta <Sayani.Gupta@monash.edu>
2021-05-20 09:29:10 +1000

    fixed wrong plots

--- paper/scripts/main.R
+++ paper/scripts/main.R
@@ -27,15 +27,15 @@ elec <- elec %>%
     wday = wday(date, label = TRUE, week_start = 1),
     weekday = if_else(wday %in% c("Sat", "Sun"), "Weekend", "Weekday")
   )
-p_cal_elec <- elec %>% 
-  filter(id %in% c(2, 4)) %>% 
-  frame_calendar(x = time, y = kwh, date = date, nrow = 1) %>% 
-  ggplot(aes(x = .time, y = .kwh, group = date)) +
-  geom_line(aes(colour = as.factor(id)), size = 0.5) +
-  scale_colour_brewer(name = "", palette = "Dark2", direction = 1) +
-  facet_grid(id ~ ., labeller = label_both) +
-  theme(legend.position = "bottom")
-prettify(p_cal_elec, size = 2.5, label.padding = unit(0.1, "lines"))
+# p_cal_elec <- elec %>% 
+#   filter(id %in% c(2, 4)) %>% 
+#   frame_calendar(x = time, y = kwh, date = date, nrow = 1) %>% 
+#   ggplot(aes(x = .time, y = .kwh, group = date)) +
+#   geom_line(aes(colour = as.factor(id)), size = 0.5) +
+#   scale_colour_brewer(name = "", palette = "Dark2", direction = 1) +
+#   facet_grid(id ~ ., labeller = label_both) +
+#   theme(legend.position = "bottom")
+# prettify(p_cal_elec, size = 2.5, label.padding = unit(0.1, "lines"))
 
 ## ---- intro_all
 id2_tsibble <- elec %>% 
@@ -101,18 +101,53 @@ ggpubr::ggarrange(p1, p2, ncol = 2)
 ## ---- id4
 ggpubr::ggarrange(p3, p4, ncol = 2)
 
+
+## ----onegran
+id2_tsibble_hd <- elec %>% 
+  filter(id == 2) %>% 
+  as_tsibble(index = date_time) %>% 
+  create_gran("hour_day") %>% 
+  ggplot(aes(x = hour_day, y = kwh)) +
+  geom_boxplot(width = 0.6, outlier.colour = "black", outlier.alpha = 0.5, fill = "#CC79A7", colour =  "#0072B2") + scale_y_log10() +
+  geom_jitter(alpha = 0.04, colour = "#E69F00")
+  
+
+id2_tsibble_dw <- elec %>% 
+  filter(id == 2) %>% 
+  as_tsibble(index = date_time) %>% 
+  create_gran("month_year") %>% 
+  ggplot(aes(x = month_year, y = kwh)) +
+  geom_boxplot(width = 0.6, outlier.colour = "black", outlier.alpha = 0.5, fill = "#CC79A7", colour =  "#0072B2") + scale_y_log10()+
+  geom_jitter(alpha = 0.04, colour = "#E69F00")
+
+
+# id2_tsibble_2gran <- id2_tsibble %>%
+#   prob_plot("month_year",
+#             "hour_day",
+#             response = "kwh",
+#             plot_type = "quantile",
+#             symmetric = TRUE,
+#             quantile_prob = c(0.1, 0.25,0.5,0.75, 0.9),palette = "Blues") + ggtitle("") 
+  
+(id2_tsibble_hd/ id2_tsibble_dw) 
+
+
 ## ---- null4by2
-sim_varall_normal <- function(nx, nfacet, mean, sd, w) {
+sim_varall_normal <- function(nx, nfacet, mean, sd, w1, w2) {
   dist_normal((mean + seq(0,
                           (nx *
                              nfacet - 1),
                           by = 1
-  ) * w), sd)
+  ) * w1), (sd + seq(0,
+                      (nx *
+                         nfacet - 1),
+                      by = 1
+  ) * w2))
 }
 sim_panel_varall <- sim_panel(
   nx = 2, nfacet = 3,
   ntimes = 500,
-  sim_dist = sim_varall_normal(2, 3, 5, 10, 5)
+  sim_dist = sim_varall_normal(2, 3, 5, 10, 5, -1.5)
 ) %>% unnest(data)
 
 p_varall <- sim_panel_varall %>%
@@ -125,14 +160,14 @@ p_varall <- sim_panel_varall %>%
   ylab("")
 
 
-sim_varx_normal <- function(nx, nfacet, mean, sd, w) {
-  rep(dist_normal((mean + seq(0, nx - 1, by = 1) * w), sd), nfacet)
+sim_varx_normal <- function(nx, nfacet, mean, sd, w1, w2) {
+  rep(dist_normal((mean + seq(0, nx - 1, by = 1) * w1), (mean + seq(0, nx - 1, by = 1) * w2)), nfacet)
 }
 
 sim_panel_varx <- sim_panel(
   nx = 2, nfacet = 3,
   ntimes = 500,
-  sim_dist = sim_varx_normal(2, 3, 5, 10, 5)
+  sim_dist = sim_varx_normal(2, 3, 5, 10, 5, -1.5)
 ) %>% unnest(data)
 
 
@@ -147,14 +182,14 @@ p_varx <- sim_panel_varx %>%
 
 
 
-sim_varf_normal <- function(nx, nfacet, mean, sd, w) {
-  rep(dist_normal((mean + seq(0, nfacet - 1, by = 1) * w), sd), each = nx)
+sim_varf_normal <- function(nx, nfacet, mean, sd, w1, w2) {
+  rep(dist_normal((mean + seq(0, nfacet - 1, by = 1) * w1), (sd + seq(0, nfacet - 1, by = 1) * w2)), each = nx)
 }
 
 sim_panel_varf <- sim_panel(
   nx = 2, nfacet = 3,
   ntimes = 500,
-  sim_dist = sim_varf_normal(2, 3, 5, 10, 5)
+  sim_dist = sim_varf_normal(2, 3, 5, 10, 5, 5)
 ) %>% unnest(data)
 
 
@@ -196,7 +231,8 @@ ggpubr::ggarrange(p_null, p_varf,  p_varx, p_varall, nrow = 2, ncol = 2,
 
 
 ## ---- notations
-d1 <- tibble (variable = c("nx", "nfacet", "$N_C$", "$H_{N_C}$", "$lambda$", "$omega$", "$wpd$","$nperm$", "$nsim$"),  description = c("number of x categories", "number of facet categories","number of cyclic granularities", "set of harmonies", "tuning parameter", "mean increment", "raw distance measure", "number of permutations for threshold/normalization", "number of simulations"))
+## 
+d1 <- tibble (variable = c("$N_C$", "$H_{N_C}$", "nx", "nfacet" , "$lambda$", "$omega$", "$wpd$","$nperm$", "$nsim$"),  description = c("number of cyclic granularities", "set of harmonies", "number of x-axis categories", "number of facet categories", "tuning parameter", "increment (mean or sd)", "raw weighted pairwise distance", "number of permutations for threshold/normalization", "number of simulations"))
 
 
 d2 <- tibble(variable =  c("$wpd_{norm}$", 
@@ -205,16 +241,19 @@ d2 <- tibble(variable =  c("$wpd_{norm}$",
                            "$D_{var_f}$", 
                            "$D_{var_x}$",
                            "$D_{var_{all}}$"), 
-             description = c("normalized distance measure",
+             description = c("normalized weighted pairwise distance",
                              "threshold for significance",
-                             "null design",
-                             "design with varying facets only", 
-                             "design with varying x only", 
-                             "design with varying both facets and x"))
+                             "null design with no distributional difference across categories",
+                             "design with distributional difference only across facets categories", 
+                             "design with distributional difference only across x-axis categories", 
+                             "design with distributional difference across both facet and x-axis"))
 
 
 d <- bind_rows(d1, d2)
-knitr::kable(d, format = "markdown", escape = FALSE, caption = "Nomenclature table")
+knitr::kable(d, format = "markdown", 
+             escape = FALSE,
+             caption = 
+               "Nomenclature table")
 
 ##----distance-explain
 knitr::include_graphics(here::here("paper/Figs/dist_explain.png"))
@@ -251,6 +290,8 @@ G21_norm %>%
 ## ---- linear-model
 G21 <- read_rds(here("simulations/raw/null_design_quantrans/data-agg/all_data_wpd_N01.rds"))
 
+
+
 G21_median <- G21 %>% 
   group_by(nx*nfacet) %>% 
   summarise(actual = median(value))
@@ -271,21 +312,62 @@ G21 %>%
   ylab("wpd normalised using linear modelling approach")
 
 
+
+
+
+
 ## ---- glm-tab
+
+
+
+G21 <- read_rds(here("simulations/raw/null_design_quantrans/data-agg/all_data_wpd_Gamma21.rds"))
+G21_median <- G21 %>% 
+  group_by(nx*nfacet) %>% 
+  summarise(actual = median(value))
+glm_fit <- glm(actual ~ log(`nx * nfacet`),
+               family = Gamma(link = "inverse"),
+               data = G21_median)
+intercept <- glm_fit$coefficients[1]
+slope <- glm_fit$coefficients[2]
+G21_sd  = G21 %>% 
+  mutate(wpd_glm =  (value - (1/(intercept + slope*log(nx*nfacet)
+  )
+  )
+  ))
+scale_fac <- 1/G21_sd$wpd_glm %>% sd()
+# checking the fit of the residuals from glm fit
+# fitted_glm <- fitted(glm_fit, type = "response")
+# residuals <- residuals.glm(glm_fit, type = "response")
+# hist(residuals)
+#h = augment(glm_fit)
+# ggplot(h) +
+#   geom_histogram(aes(x = .resid))
+# residual <- G21_median$actual  - fitted_glm
+# 
+# hist(residual)
+# G21 %>% 
+#   ggplot(aes(x=log(nx*nfacet),
+#              y = (value - (1/(intercept + slope*log(nx*nfacet)
+#              )
+#              )
+#              )
+#   )
+#   ) +
+#   geom_point() + stat_summary(fun=mean, geom="line", aes(group=1), color = "blue") + 
+#   ylab("wpd_glm = wpd_raw - 1/(a  + b*log(nx*nfacet))")
+
 broom::tidy(glm_fit) %>% kable(caption = "Results of generalised linear model to capture the relationship between wpd and number of comparisons.")
 
 
 ## ---- wpd-glm-dist
+
 G21_glm <- G21 %>% 
   mutate(wpd_glm =  (value - (1/(intercept + slope*log(nx*nfacet)
   )
   )
   ),
   wpd_glm_scaled = ((wpd_glm*320)))
-
-#G21_glm$wpd_glm_scaled %>% sd()
-
-
+G21_glm$wpd_glm_scaled %>% sd()
 G21_glm %>% 
   ggplot() +
   geom_density(aes(x = wpd_glm), 
@@ -294,7 +376,8 @@ G21_glm %>%
              labeller = "label_both") +
   theme(legend.position = "bottom") 
 
-##----hist-qq
+
+##----hist-qq-new
 hist <- ggplot(G21_glm) +
   geom_histogram(aes(x = wpd_glm_scaled)) +
   ggtitle("a") + xlab("transformed wpd using glm approach")
@@ -313,10 +396,11 @@ qqplot <-  p + geom_qq() + geom_qq_line() + coord_fixed(ratio = 4/5) +
 
 # G21_glm$wpd_glm_scaled %>% 
 #   range()
+library(patchwork)
 hist + qqplot
 
 ##----dist-new-same-scale-link
-G21_permutation <- read_csv(here("simulations/norm/null_design_quantrans_nperm/data-agg/all_data_wpd_N01.csv")) %>%
+G21_permutation <- read_rds(here("simulations/norm/null_design_quantrans_nperm/data-agg/all_data_wpd_N01.rds")) %>%
   rename("wpd_permutation" = "value")
 
 
