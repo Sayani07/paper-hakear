887f8eb9a1d1fb1f57898f67e054d2fda7c224db
Rob J Hyndman <robjhyndman@gmail.com>
2021-10-29 15:19:27 +1100

    Improved Sec 3 and notational consistency

--- paper/scripts/main.R
+++ paper/scripts/main.R
@@ -572,12 +572,15 @@ glm_fit_onegran  <- glm(actual ~ log(`nx * nfacet`),
                data = G21_median_onegran)
 
 bind_rows(broom::tidy(glm_fit_onegran),
-          tidy(glm_fit), .id = "m") %>%
+          tidy(glm_fit), .id = "$m$") %>%
   mutate(estimate = round(estimate, 2),
          std.error = round(std.error, 2),
          statistic = round(statistic, 2),
-         p.value = round(p.value, 2)
-         ) %>% kable(caption = "Results of generalised linear model to capture the relationship between $wpd_{raw}$ and number of comparisons.")
+         p.value = round(p.value, 2),
+         term = rep(c("Intercept","$\\log(\\nx \\times \\nf)$"),2)
+         ) %>%
+  kable(booktabs=TRUE, escape=FALSE,
+    caption = "Results of generalised linear model to capture the relationship between $\\wpdsub{raw}$ and the number of comparisons.")
 
 
 ## ---- wpd-glm-dist
